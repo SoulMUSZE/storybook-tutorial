8425fb384cd0a62aad2c577c45eb67cb9a690c0d
Husni Wahab <mdhusni@gmail.com>
2019-06-28 10:58:46 +0800

    viewport tab not showing

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,10 @@
 // .storybook/config.js
 
-import { configure, addDecorator } from '@storybook/react';
+import { configure, addDecorator, addParameters } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs/react';
 
 addDecorator(withKnobs);
+addParameters({ viewport: { defaultViewport: "iphone6" } })
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /\.stories\.js$/);
